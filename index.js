d95f3be6aa1e83d2991d8fc3a56fdaff2a643c57
K.Akhila <kodipaka.akhila223@gmail.com>
2017-01-07 11:55:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'isomorphic-fetch';
+
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
